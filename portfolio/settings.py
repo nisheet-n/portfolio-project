b10088fd5448d60642e5b7f637f6feff7c30cb33
nisheet-n <nisheet2000@gmail.com>
2020-10-22 22:04:29 +0530

    Added some data

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -117,6 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio/static/'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = '/static/'
 
